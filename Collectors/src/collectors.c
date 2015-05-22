d4ada3e789c55e3a5bb2a947073d78e120b6da9e
OnyxDragon <chewtjiahwei@gmail.com>
2015-05-22 10:53:29 +0800

    Update collectors.c

--- Collectors/src/collectors.c
+++ Collectors/src/collectors.c
@@ -2,9 +2,9 @@
 
 /*
  CITS3002 Project 2015
- Name(s):             Benjamin Sinclair
- Student number(s):   20153423
- Date:
+ Name(s):             Benjamin Sinclair, Tjia Hwei Chew, Zhiyang Zhang, Jordan King
+ Student number(s):   20153423, 21021956, 20830178, 21118935
+ Date:                22nd May 2015
  */
 
 
